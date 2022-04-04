7c1180719ed3bda533a7798efd67e8b18ae4e33e
Karthik Venkatesh <kvenkate@tcd.ie>
2022-04-04 16:58:30 +0100

    part 2 init

---
+++ BusManagementSystem/src/TST.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import org.w3c.dom.Node;
+
+public class TST {
+    private node root;
+    private ArrayList<String> al;
+
+    public TST(){
+        root = null;
+    }
+
+    public class node {
+        private char c;
+        private node left, mid, right;
+
+        public node(char c){
+            this.c = c;
+            this.left = left;
+            this.mid = mid;
+            this.right = right;
+        }
+        
+    }
+
+}
---
+++ BusManagementSystem/src/q2.java
@@ -0,0 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+
+public class q2 {
+    public static void main(String[] args) throws IOException{
+        File stopsFile = new File();
+    }
+}
