9e01710074c6ccd41f821f0b2a543fa0da09dbdf
Karthik Venkatesh <kvenkate@tcd.ie>
2022-04-08 02:50:09 +0100

    part4 iniit

---
+++ BusManagementSystem/src/node.java
@@ -0,0 +1,14 @@
+class node {
+    public char c;
+    public node left, mid, right;
+    boolean bool;
+
+    public node(char c){
+        this.c = c;
+        this.left = left;
+        this.mid = mid;
+        this.right = right;
+        this.bool = false;
+    }
+    
+}
